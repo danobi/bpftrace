danobi/bpftrace

commit 7c40e2a36d705ebfa6cbf965e65d041436e0ce70

build: Share core bpftrace code with test binary
Before, we had a lot of duplicated cmake code between `bpftrace_test`
and `bpftrace`. This was, as they say in the industry, doubly bad,
because you had to write the same cmake code twice and the build would
compile the same code twice.

With this change, there is less duplicated cmake code and the build runs
much faster (both cold starts):

Before:
    609.40s user 30.70s system 329% cpu 3:14.13 total

After:
    422.81s user 21.66s system 300% cpu 2:28.07 total

This commit also fixes a cyclic dependency: ast.a depended on functions
in libbpftrace.a (`signal_name_to_num` and `FakeMap`). Better not to
have cyclic dependencies (ugly and may cause issues), so fix by moving
source into ast/ directory.

src/CMakeLists.txt
@@ -11,7 +11,7 @@ else()
   set(BPFTRACE bpftrace)
 endif()
 
-add_executable(${BPFTRACE}
+add_library(libbpftrace
   attached_probe.cpp
   bpffeature.cpp
   bpforc.cpp
@@ -21,7 +21,6 @@ add_executable(${BPFTRACE}
   clang_parser.cpp
   disasm.cpp
   driver.cpp
-  fake_map.cpp
   lockdown.cpp
   log.cpp
   map.cpp
@@ -31,15 +30,21 @@ add_executable(${BPFTRACE}
   procmon.cpp
   printf.cpp
   resolve_cgroupid.cpp
-  signal.cpp
   struct.cpp
   tracepoint_format_parser.cpp
   types.cpp
   usdt.cpp
   utils.cpp
   ${BFD_DISASM_SRC}
+)
+# So it's not "liblibbpftrace"
+set_target_properties(libbpftrace PROPERTIES PREFIX "")
+
+add_executable(${BPFTRACE}
   ${MAIN_SRC}
 )
+
+target_link_libraries(${BPFTRACE} libbpftrace)
 
 if (BUILD_FUZZ)
   target_compile_options(${BPFTRACE} PUBLIC "-DFUZZ")
@@ -63,94 +68,94 @@ if (BUILD_FUZZ)
 endif()
 
 if(HAVE_NAME_TO_HANDLE_AT)
-  target_compile_definitions(${BPFTRACE} PRIVATE HAVE_NAME_TO_HANDLE_AT=1)
+  target_compile_definitions(libbpftrace PRIVATE HAVE_NAME_TO_HANDLE_AT=1)
 endif(HAVE_NAME_TO_HANDLE_AT)
 if(HAVE_BCC_PROG_LOAD)
-  target_compile_definitions(${BPFTRACE} PRIVATE HAVE_BCC_PROG_LOAD)
+  target_compile_definitions(libbpftrace PRIVATE HAVE_BCC_PROG_LOAD)
 endif(HAVE_BCC_PROG_LOAD)
 if(HAVE_BCC_CREATE_MAP)
-  target_compile_definitions(${BPFTRACE} PRIVATE HAVE_BCC_CREATE_MAP)
+  target_compile_definitions(libbpftrace PRIVATE HAVE_BCC_CREATE_MAP)
 endif(HAVE_BCC_CREATE_MAP)
 if(HAVE_BCC_ELF_FOREACH_SYM)
-  target_compile_definitions(${BPFTRACE} PRIVATE HAVE_BCC_ELF_FOREACH_SYM)
+  target_compile_definitions(libbpftrace PRIVATE HAVE_BCC_ELF_FOREACH_SYM)
 endif(HAVE_BCC_ELF_FOREACH_SYM)
 if(HAVE_BCC_USDT_ADDSEM)
-  target_compile_definitions(${BPFTRACE} PRIVATE HAVE_BCC_USDT_ADDSEM)
+  target_compile_definitions(libbpftrace PRIVATE HAVE_BCC_USDT_ADDSEM)
 endif(HAVE_BCC_USDT_ADDSEM)
 if (LIBBPF_BTF_DUMP_FOUND)
-  target_compile_definitions(${BPFTRACE} PRIVATE HAVE_LIBBPF_BTF_DUMP)
+  target_compile_definitions(libbpftrace PRIVATE HAVE_LIBBPF_BTF_DUMP)
   target_include_directories(${BPFTRACE} PUBLIC ${LIBBPF_INCLUDE_DIRS})
-  target_link_libraries(${BPFTRACE} ${LIBBPF_LIBRARIES})
+  target_link_libraries(libbpftrace ${LIBBPF_LIBRARIES})
   if (HAVE_LIBBPF_BTF_DUMP_EMIT_TYPE_DECL)
-    target_compile_definitions(${BPFTRACE} PRIVATE HAVE_LIBBPF_BTF_DUMP_EMIT_TYPE_DECL)
+    target_compile_definitions(libbpftrace PRIVATE HAVE_LIBBPF_BTF_DUMP_EMIT_TYPE_DECL)
   endif()
 endif(LIBBPF_BTF_DUMP_FOUND)
 if (LIBBPF_FOUND)
-  target_compile_definitions(${BPFTRACE} PRIVATE HAVE_LIBBPF)
+  target_compile_definitions(libbpftrace PRIVATE HAVE_LIBBPF)
 endif(LIBBPF_FOUND)
 
 if (HAVE_LIBBPF_MAP_BATCH)
-  target_compile_definitions(${BPFTRACE} PRIVATE HAVE_LIBBPF_MAP_BATCH)
+  target_compile_definitions(libbpftrace PRIVATE HAVE_LIBBPF_MAP_BATCH)
 endif()
 
 if (HAVE_BCC_KFUNC)
-  target_compile_definitions(${BPFTRACE} PRIVATE HAVE_BCC_KFUNC)
+  target_compile_definitions(libbpftrace PRIVATE HAVE_BCC_KFUNC)
 endif(HAVE_BCC_KFUNC)
 if(HAVE_BFD_DISASM)
-  target_compile_definitions(${BPFTRACE} PRIVATE HAVE_BFD_DISASM)
+  target_compile_definitions(libbpftrace PRIVATE HAVE_BFD_DISASM)
   if(LIBBFD_DISASM_FOUR_ARGS_SIGNATURE)
-    target_compile_definitions(${BPFTRACE} PRIVATE LIBBFD_DISASM_FOUR_ARGS_SIGNATURE)
+    target_compile_definitions(libbpftrace PRIVATE LIBBFD_DISASM_FOUR_ARGS_SIGNATURE)
   endif(LIBBFD_DISASM_FOUR_ARGS_SIGNATURE)
   if(STATIC_LINKING)
     add_library(LIBBFD STATIC IMPORTED)
     set_property(TARGET LIBBFD PROPERTY IMPORTED_LOCATION ${LIBBFD_LIBRARIES})
-    target_link_libraries(${BPFTRACE} LIBBFD)
+    target_link_libraries(libbpftrace LIBBFD)
     add_library(LIBOPCODES STATIC IMPORTED)
     set_property(TARGET LIBOPCODES PROPERTY IMPORTED_LOCATION ${LIBOPCODES_LIBRARIES})
-    target_link_libraries(${BPFTRACE} LIBOPCODES)
+    target_link_libraries(libbpftrace LIBOPCODES)
     add_library(LIBIBERTY STATIC IMPORTED)
     set_property(TARGET LIBIBERTY PROPERTY IMPORTED_LOCATION ${LIBIBERTY_LIBRARIES})
-    target_link_libraries(${BPFTRACE} LIBIBERTY)
+    target_link_libraries(libbpftrace LIBIBERTY)
   else()
-    target_link_libraries(${BPFTRACE} ${LIBBFD_LIBRARIES})
-    target_link_libraries(${BPFTRACE} ${LIBOPCODES_LIBRARIES})
+    target_link_libraries(libbpftrace ${LIBBFD_LIBRARIES})
+    target_link_libraries(libbpftrace ${LIBOPCODES_LIBRARIES})
   endif(STATIC_LINKING)
 endif(HAVE_BFD_DISASM)
 if(LIBBCC_ATTACH_KPROBE_SIX_ARGS_SIGNATURE)
-  target_compile_definitions(${BPFTRACE} PRIVATE LIBBCC_ATTACH_KPROBE_SIX_ARGS_SIGNATURE)
+  target_compile_definitions(libbpftrace PRIVATE LIBBCC_ATTACH_KPROBE_SIX_ARGS_SIGNATURE)
 endif(LIBBCC_ATTACH_KPROBE_SIX_ARGS_SIGNATURE)
 if(LIBBCC_ATTACH_UPROBE_SEVEN_ARGS_SIGNATURE)
-  target_compile_definitions(${BPFTRACE} PRIVATE LIBBCC_ATTACH_UPROBE_SEVEN_ARGS_SIGNATURE)
+  target_compile_definitions(libbpftrace PRIVATE LIBBCC_ATTACH_UPROBE_SEVEN_ARGS_SIGNATURE)
 endif(LIBBCC_ATTACH_UPROBE_SEVEN_ARGS_SIGNATURE)
 
 if (ALLOW_UNSAFE_PROBE)
-  target_compile_definitions(${BPFTRACE} PRIVATE HAVE_UNSAFE_PROBE)
+  target_compile_definitions(libbpftrace PRIVATE HAVE_UNSAFE_PROBE)
 endif(ALLOW_UNSAFE_PROBE)
 
-target_link_libraries(${BPFTRACE} arch ast parser resources)
+target_link_libraries(libbpftrace arch ast parser resources)
 
-target_link_libraries(${BPFTRACE} ${LIBBCC_LIBRARIES})
+target_link_libraries(libbpftrace ${LIBBCC_LIBRARIES})
 if(STATIC_LINKING)
   # These are not part of the static libbcc so have to be added separate
-  target_link_libraries(${BPFTRACE} ${LIBBCC_BPF_LIBRARY_STATIC})
-  target_link_libraries(${BPFTRACE} ${LIBBPF_LIBRARIES})
-  target_link_libraries(${BPFTRACE} ${LIBBCC_LOADER_LIBRARY_STATIC})
+  target_link_libraries(libbpftrace ${LIBBCC_BPF_LIBRARY_STATIC})
+  target_link_libraries(libbpftrace ${LIBBPF_LIBRARIES})
+  target_link_libraries(libbpftrace ${LIBBCC_LOADER_LIBRARY_STATIC})
 
   add_library(LIBELF STATIC IMPORTED)
   set_property(TARGET LIBELF PROPERTY IMPORTED_LOCATION ${LIBELF_LIBRARIES})
-  target_link_libraries(${BPFTRACE} LIBELF)
+  target_link_libraries(libbpftrace LIBELF)
 
   if(NOT STATIC_LIBC)
     set_target_properties(${BPFTRACE} PROPERTIES LINK_FLAGS "${EMBEDDED_LINK_FLAGS}")
   endif()
 else()
-  target_link_libraries(${BPFTRACE} ${LIBELF_LIBRARIES})
+  target_link_libraries(libbpftrace ${LIBELF_LIBRARIES})
 endif(STATIC_LINKING)
 
 # Support for std::filesystem
 # GCC version <9 and Clang (all versions) require -lstdc++fs
 if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang" OR ${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS "9")
-  target_link_libraries(${BPFTRACE} "stdc++fs")
+  target_link_libraries(libbpftrace "stdc++fs")
 endif()
 
 if (BUILD_ASAN)
@@ -180,7 +185,7 @@ install(TARGETS ${BPFTRACE} DESTINATION ${CMAKE_INSTALL_BINDIR})
 set(KERNEL_HEADERS_DIR "" CACHE PATH "Hard-code kernel headers directory")
 if (KERNEL_HEADERS_DIR)
   MESSAGE(STATUS "Using KERNEL_HEADERS_DIR=${KERNEL_HEADERS_DIR}")
-  target_compile_definitions(${BPFTRACE} PUBLIC KERNEL_HEADERS_DIR="${KERNEL_HEADERS_DIR}")
+  target_compile_definitions(libbpftrace PUBLIC KERNEL_HEADERS_DIR="${KERNEL_HEADERS_DIR}")
 endif()
 
 execute_process(

src/ast/CMakeLists.txt
@@ -1,12 +1,14 @@
 add_library(ast
   ast.cpp
   attachpoint_parser.cpp
+  callback_visitor.cpp
   codegen_llvm.cpp
+  fake_map.cpp
   field_analyser.cpp
   irbuilderbpf.cpp
   printer.cpp
   semantic_analyser.cpp
-  callback_visitor.cpp
+  signal.cpp
   visitors.cpp
 )
 

tests/CMakeLists.txt
@@ -45,88 +45,10 @@ add_executable(bpftrace_test
   utils.cpp
 
   ${CODEGEN_SRC}
-
-  ${CMAKE_SOURCE_DIR}/src/attached_probe.cpp
-  ${CMAKE_SOURCE_DIR}/src/bpftrace.cpp
-  ${CMAKE_SOURCE_DIR}/src/bpffeature.cpp
-  ${CMAKE_SOURCE_DIR}/src/bpforc.cpp
-  ${CMAKE_SOURCE_DIR}/src/btf.cpp
-  ${CMAKE_SOURCE_DIR}/src/child.cpp
-  ${CMAKE_SOURCE_DIR}/src/clang_parser.cpp
-  ${CMAKE_SOURCE_DIR}/src/disasm.cpp
-  ${CMAKE_SOURCE_DIR}/src/driver.cpp
-  ${CMAKE_SOURCE_DIR}/src/fake_map.cpp
-  ${CMAKE_SOURCE_DIR}/src/log.cpp
-  ${CMAKE_SOURCE_DIR}/src/map.cpp
-  ${CMAKE_SOURCE_DIR}/src/mapkey.cpp
-  ${CMAKE_SOURCE_DIR}/src/output.cpp
-  ${CMAKE_SOURCE_DIR}/src/printf.cpp
-  ${CMAKE_SOURCE_DIR}/src/probe_matcher.cpp
-  ${CMAKE_SOURCE_DIR}/src/procmon.cpp
-  ${CMAKE_SOURCE_DIR}/src/resolve_cgroupid.cpp
-  ${CMAKE_SOURCE_DIR}/src/signal.cpp
-  ${CMAKE_SOURCE_DIR}/src/struct.cpp
-  ${CMAKE_SOURCE_DIR}/src/tracepoint_format_parser.cpp
-  ${CMAKE_SOURCE_DIR}/src/types.cpp
-  ${CMAKE_SOURCE_DIR}/src/usdt.cpp
-  ${CMAKE_SOURCE_DIR}/src/utils.cpp
-  ${BFD_DISASM_SRC}
 )
 
-if (HAVE_LIBBPF_MAP_BATCH)
-  target_compile_definitions(bpftrace_test PRIVATE HAVE_LIBBPF_MAP_BATCH)
-endif()
-
-if(HAVE_NAME_TO_HANDLE_AT)
-  target_compile_definitions(bpftrace_test PRIVATE HAVE_NAME_TO_HANDLE_AT=1)
-endif(HAVE_NAME_TO_HANDLE_AT)
-if(HAVE_BCC_PROG_LOAD)
-  target_compile_definitions(bpftrace_test PRIVATE HAVE_BCC_PROG_LOAD)
-endif(HAVE_BCC_PROG_LOAD)
-if(HAVE_BCC_CREATE_MAP)
-  target_compile_definitions(bpftrace_test PRIVATE HAVE_BCC_CREATE_MAP)
-endif(HAVE_BCC_CREATE_MAP)
-if (LIBBPF_BTF_DUMP_FOUND)
-  target_compile_definitions(bpftrace_test PRIVATE HAVE_LIBBPF_BTF_DUMP)
-  target_include_directories(bpftrace_test PUBLIC ${LIBBPF_INCLUDE_DIRS})
-  target_link_libraries(bpftrace_test ${LIBBPF_LIBRARIES})
-  if (HAVE_LIBBPF_BTF_DUMP_EMIT_TYPE_DECL)
-    target_compile_definitions(bpftrace_test PRIVATE HAVE_LIBBPF_BTF_DUMP_EMIT_TYPE_DECL)
-  endif()
-endif(LIBBPF_BTF_DUMP_FOUND)
-if (HAVE_BCC_KFUNC)
-  target_compile_definitions(bpftrace_test PRIVATE HAVE_BCC_KFUNC)
-endif(HAVE_BCC_KFUNC)
-if(HAVE_BCC_USDT_ADDSEM)
-  target_compile_definitions(bpftrace PRIVATE HAVE_BCC_USDT_ADDSEM)
-endif(HAVE_BCC_USDT_ADDSEM)
 target_compile_definitions(bpftrace_test PRIVATE TEST_CODEGEN_LOCATION="${CMAKE_SOURCE_DIR}/tests/codegen/llvm/")
-if(HAVE_BFD_DISASM)
-  target_compile_definitions(bpftrace_test PRIVATE HAVE_BFD_DISASM)
-  if(LIBBFD_DISASM_FOUR_ARGS_SIGNATURE)
-    target_compile_definitions(bpftrace_test PRIVATE LIBBFD_DISASM_FOUR_ARGS_SIGNATURE)
-  endif(LIBBFD_DISASM_FOUR_ARGS_SIGNATURE)
-  if(STATIC_LINKING)
-    add_library(LIBBFD STATIC IMPORTED)
-    set_property(TARGET LIBBFD PROPERTY IMPORTED_LOCATION ${LIBBFD_LIBRARIES})
-    target_link_libraries(bpftrace_test LIBBFD)
-    add_library(LIBOPCODES STATIC IMPORTED)
-    set_property(TARGET LIBOPCODES PROPERTY IMPORTED_LOCATION ${LIBOPCODES_LIBRARIES})
-    target_link_libraries(bpftrace_test LIBOPCODES)
-    add_library(LIBIBERTY STATIC IMPORTED)
-    set_property(TARGET LIBIBERTY PROPERTY IMPORTED_LOCATION ${LIBIBERTY_LIBRARIES})
-    target_link_libraries(bpftrace_test LIBIBERTY)
-  else()
-    target_link_libraries(bpftrace_test ${LIBBFD_LIBRARIES})
-    target_link_libraries(bpftrace_test ${LIBOPCODES_LIBRARIES})
-  endif(STATIC_LINKING)
-endif(HAVE_BFD_DISASM)
-if(LIBBCC_ATTACH_KPROBE_SIX_ARGS_SIGNATURE)
-  target_compile_definitions(bpftrace_test PRIVATE LIBBCC_ATTACH_KPROBE_SIX_ARGS_SIGNATURE)
-endif(LIBBCC_ATTACH_KPROBE_SIX_ARGS_SIGNATURE)
-if(LIBBCC_ATTACH_UPROBE_SEVEN_ARGS_SIGNATURE)
-  target_compile_definitions(bpftrace_test PRIVATE LIBBCC_ATTACH_UPROBE_SEVEN_ARGS_SIGNATURE)
-endif(LIBBCC_ATTACH_UPROBE_SEVEN_ARGS_SIGNATURE)
+target_link_libraries(bpftrace_test libbpftrace)
 
 if(CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64")
   target_compile_definitions(bpftrace_test PRIVATE ARCH_AARCH64)
@@ -140,34 +62,14 @@ elseif(CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64")
   target_compile_definitions(bpftrace_test PRIVATE ARCH_X86_64)
 endif()
 
-target_link_libraries(bpftrace_test arch ast parser resources)
-
-target_link_libraries(bpftrace_test ${LIBBCC_LIBRARIES})
-if (STATIC_LINKING)
+if(STATIC_LINKING)
   if(EMBED_LLVM OR EMBED_CLANG)
     set_target_properties(bpftrace_test PROPERTIES LINK_FLAGS "${EMBEDDED_LINK_FLAGS}")
   endif()
   if(STATIC_LIBC)
     set_target_properties(bpftrace_test PROPERTIES LINK_FLAGS "-static")
   endif()
-
-  # These are not part of the static libbcc so have to be added separate
-  target_link_libraries(bpftrace_test ${LIBBCC_BPF_LIBRARY_STATIC})
-  target_link_libraries(bpftrace_test ${LIBBPF_LIBRARIES})
-  target_link_libraries(bpftrace_test ${LIBBCC_LOADER_LIBRARY_STATIC})
-
-  add_library(LIBELF STATIC IMPORTED)
-  set_property(TARGET LIBELF PROPERTY IMPORTED_LOCATION ${LIBELF_LIBRARIES})
-  target_link_libraries(bpftrace_test LIBELF)
-else()
-  target_link_libraries(bpftrace_test ${LIBELF_LIBRARIES})
 endif(STATIC_LINKING)
-
-# Support for std::filesystem
-# GCC version <9 and Clang (all versions) require -lstdc++fs
-if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang" OR ${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS "9")
-  target_link_libraries(bpftrace_test "stdc++fs")
-endif()
 
 find_package(Threads REQUIRED)
 

src/ast/codegen_llvm.cpp
@@ -6,6 +6,7 @@
 #include "codegen_helper.h"
 #include "log.h"
 #include "parser.tab.hh"
+#include "signal_bt.h"
 #include "tracepoint_format_parser.h"
 #include "types.h"
 #include "usdt.h"

src/ast/fake_map.cpp
@@ -35,5 +35,4 @@ FakeMap::FakeMap(enum bpf_map_type map_type __attribute__((unused)))
   mapfd_ = next_mapfd_++;
 }
 
-
 } // namespace bpftrace

src/ast/semantic_analyser.cpp
@@ -6,6 +6,7 @@
 #include "parser.tab.hh"
 #include "printf.h"
 #include "probe_matcher.h"
+#include "signal_bt.h"
 #include "tracepoint_format_parser.h"
 #include "usdt.h"
 #include <algorithm>

src/ast/signal.cpp
@@ -1,0 +1,43 @@
+#include "signal_bt.h"
+
+#include <algorithm>
+#include <map>
+
+namespace bpftrace {
+
+static std::map<std::string, int> signals = {
+  { "SIGABRT", SIGABRT },     { "SIGALRM", SIGALRM },   { "SIGBUS", SIGBUS },
+  { "SIGCHLD", SIGCHLD },     { "SIGCONT", SIGCONT },   { "SIGFPE", SIGFPE },
+  { "SIGHUP", SIGHUP },       { "SIGILL", SIGILL },     { "SIGINT", SIGINT },
+  { "SIGKILL", SIGKILL },     { "SIGPIPE", SIGPIPE },   { "SIGPOLL", SIGPOLL },
+  { "SIGQUIT", SIGQUIT },     { "SIGSEGV", SIGSEGV },   { "SIGSTOP", SIGSTOP },
+  { "SIGSYS", SIGSYS },       { "SIGTERM", SIGTERM },   { "SIGTRAP", SIGTRAP },
+  { "SIGTSTP", SIGTSTP },     { "SIGTTIN", SIGTTIN },   { "SIGTTOU", SIGTTOU },
+  { "SIGURG", SIGURG },       { "SIGUSR1", SIGUSR1 },   { "SIGUSR2", SIGUSR2 },
+  { "SIGVTALRM", SIGVTALRM }, { "SIGWINCH", SIGWINCH }, { "SIGXCPU", SIGXCPU },
+  { "SIGXFSZ", SIGXFSZ },
+};
+
+int signal_name_to_num(const std::string &signal)
+{
+  if (signal.empty())
+  {
+    return -1;
+  }
+
+  std::string sig(signal);
+
+  std::for_each(sig.begin(), sig.end(), [](char &c) { c = ::toupper(c); });
+
+  if (sig[0] != 'S')
+  {
+    sig.insert(0, "SIG");
+  }
+
+  auto s = signals.find(sig);
+  if (s != signals.end())
+    return s->second;
+  return -1;
+}
+
+} // namespace bpftrace

commit 814b5bf5841cc2c473003dcc2dbedca3f16d86df

Split out build info into another module
7c40e2a3 ("build: Share core bpftrace code with test binary")
broke the libbpf build info b/c libbpftrace target wasn't given
the libbpf cmake vars.

This commit splits out the buildinfo such that the buildinfo is only
needed inside libbpftrace target. This is nicer b/c then we don't have
to pass cmake vars to multiple targets. It's also nicer b/c main.cpp
is less concerned with internal details like build flags.

Also note that this commit also defines LLVM_VERSION_* preprocessor
variables. I'm not sure where those variables were coming from (maybe
one of the llvm header files). It's probably better we define it
ourselves so things are more explicit.

CMakeLists.txt
@@ -161,6 +161,10 @@ else()
   add_definitions(${LLVM_DEFINITIONS})
 endif()
 
+add_definitions(-DLLVM_VERSION_MAJOR=${LLVM_VERSION_MAJOR})
+add_definitions(-DLLVM_VERSION_MINOR=${LLVM_VERSION_MINOR})
+add_definitions(-DLLVM_VERSION_PATCH=${LLVM_VERSION_PATCH})
+
 if(${LLVM_VERSION_MAJOR} VERSION_GREATER_EQUAL 11)
   set(LLVM_ORC_V2)
   add_definitions(-DLLVM_ORC_V2)

cmake/embed/embed_llvm.cmake
@@ -22,16 +22,22 @@ endif()
 if(${LLVM_VERSION} VERSION_GREATER_EQUAL "9")
   set(LLVM_FULL_VERSION "9.0.1")
   set(LLVM_VERSION_MAJOR "9")
+  set(LLVM_VERSION_MINOR "0")
+  set(LLVM_VERSION_PATCH "1")
   set(LLVM_DOWNLOAD_URL "https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_FULL_VERSION}/llvm-${LLVM_FULL_VERSION}.src.tar.xz")
   set(LLVM_URL_CHECKSUM "SHA256=00a1ee1f389f81e9979f3a640a01c431b3021de0d42278f6508391a2f0b81c9a")
 elseif(${LLVM_VERSION} VERSION_GREATER_EQUAL "8")
   set(LLVM_FULL_VERSION "8.0.1")
   set(LLVM_VERSION_MAJOR "8")
+  set(LLVM_VERSION_MINOR "0")
+  set(LLVM_VERSION_PATCH "1")
   set(LLVM_DOWNLOAD_URL "https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_FULL_VERSION}/llvm-${LLVM_FULL_VERSION}.src.tar.xz")
   set(LLVM_URL_CHECKSUM "SHA256=44787a6d02f7140f145e2250d56c9f849334e11f9ae379827510ed72f12b75e7")
 elseif(${LLVM_VERSION} VERSION_GREATER_EQUAL "7")
   set(LLVM_FULL_VERSION "7.1.0")
   set(LLVM_VERSION_MAJOR "7")
+  set(LLVM_VERSION_MINOR "1")
+  set(LLVM_VERSION_PATCH "0")
   set(LLVM_DOWNLOAD_URL "https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_FULL_VERSION}/llvm-${LLVM_FULL_VERSION}.src.tar.xz")
   set(LLVM_URL_CHECKSUM "SHA256=1bcc9b285074ded87b88faaedddb88e6b5d6c331dfcfb57d7f3393dd622b3764")
 else()

src/CMakeLists.txt
@@ -17,6 +17,7 @@ add_library(libbpftrace
   bpforc.cpp
   bpftrace.cpp
   btf.cpp
+  build_info.cpp
   child.cpp
   clang_parser.cpp
   disasm.cpp

src/build_info.cpp
@@ -1,0 +1,76 @@
+#include <sstream>
+
+#include "build_info.h"
+
+namespace bpftrace {
+
+std::string BuildInfo::report()
+{
+  std::stringstream buf;
+
+  buf << "Build" << std::endl
+      << "  version: " << BPFTRACE_VERSION << std::endl
+      << "  LLVM: " << LLVM_VERSION_MAJOR << "." << LLVM_VERSION_MINOR << "."
+      << LLVM_VERSION_PATCH << std::endl
+#ifdef LLVM_ORC_V2
+      << "  ORC: v2" << std::endl
+#endif
+      << "  foreach_sym: "
+#ifdef HAVE_BCC_ELF_FOREACH_SYM
+      << "yes" << std::endl
+#else
+      << "no" << std::endl
+#endif
+      << "  unsafe uprobe: "
+#ifdef HAVE_UNSAFE_UPROBE
+      << "yes" << std::endl;
+#else
+      << "no" << std::endl;
+#endif
+  buf << "  bfd: "
+#ifdef HAVE_BFD_DISASM
+      << "yes" << std::endl;
+#else
+      << "no" << std::endl;
+#endif
+  buf << "  bpf_attach_kfunc: "
+#ifdef HAVE_BCC_KFUNC
+      << "yes" << std::endl;
+#else
+      << "no" << std::endl;
+#endif
+  buf << "  bcc_usdt_addsem: "
+#ifdef HAVE_BCC_USDT_ADDSEM
+      << "yes" << std::endl;
+#else
+      << "no" << std::endl;
+#endif
+  buf << "  bcc bpf_attach_uprobe refcount: "
+#ifdef LIBBCC_ATTACH_UPROBE_SEVEN_ARGS_SIGNATURE
+      << "yes" << std::endl;
+#else
+      << "no" << std::endl;
+#endif
+  buf << "  libbpf: "
+#ifdef HAVE_LIBBPF
+      << "yes" << std::endl;
+#else
+      << "no" << std::endl;
+#endif
+  buf << "  libbpf btf dump: "
+#ifdef HAVE_LIBBPF_BTF_DUMP
+      << "yes" << std::endl;
+#else
+      << "no" << std::endl;
+#endif
+  buf << "  libbpf btf dump type decl: "
+#ifdef HAVE_LIBBPF_BTF_DUMP_EMIT_TYPE_DECL
+      << "yes" << std::endl;
+#else
+      << "no" << std::endl;
+#endif
+
+  return buf.str();
+}
+
+} // namespace bpftrace

src/build_info.h
@@ -1,0 +1,13 @@
+#pragma once
+
+#include <string>
+
+namespace bpftrace {
+
+class BuildInfo
+{
+public:
+  static std::string report();
+};
+
+} // namespace bpftrace

src/main.cpp
@@ -16,6 +16,7 @@
 #include "bpffeature.h"
 #include "bpforc.h"
 #include "bpftrace.h"
+#include "build_info.h"
 #include "child.h"
 #include "clang_parser.h"
 #include "codegen_llvm.h"
@@ -137,71 +138,10 @@ static int info()
             << utsname.version << std::endl
             << "  Arch: " << utsname.machine << std::endl;
 
-  std::cerr << std::endl
-            << "Build" << std::endl
-            << "  version: " << BPFTRACE_VERSION << std::endl
-            << "  LLVM: " << LLVM_VERSION_MAJOR << "." << LLVM_VERSION_MINOR
-            << "." << LLVM_VERSION_PATCH << std::endl
-#ifdef LLVM_ORC_V2
-            << "  ORC: v2" << std::endl
-#endif
-            << "  foreach_sym: "
-#ifdef HAVE_BCC_ELF_FOREACH_SYM
-            << "yes" << std::endl
-#else
-            << "no" << std::endl
-#endif
-            << "  unsafe uprobe: "
-#ifdef HAVE_UNSAFE_UPROBE
-            << "yes" << std::endl;
-#else
-            << "no" << std::endl;
-#endif
-  std::cerr << "  bfd: "
-#ifdef HAVE_BFD_DISASM
-            << "yes" << std::endl;
-#else
-            << "no" << std::endl;
-#endif
-  std::cerr << "  bpf_attach_kfunc: "
-#ifdef HAVE_BCC_KFUNC
-            << "yes" << std::endl;
-#else
-            << "no" << std::endl;
-#endif
-  std::cerr << "  bcc_usdt_addsem: "
-#ifdef HAVE_BCC_USDT_ADDSEM
-            << "yes" << std::endl;
-#else
-            << "no" << std::endl;
-#endif
-  std::cerr << "  bcc bpf_attach_uprobe refcount: "
-#ifdef LIBBCC_ATTACH_UPROBE_SEVEN_ARGS_SIGNATURE
-            << "yes" << std::endl;
-#else
-            << "no" << std::endl;
-#endif
-  std::cerr << "  libbpf: "
-#ifdef HAVE_LIBBPF
-            << "yes" << std::endl;
-#else
-            << "no" << std::endl;
-#endif
-  std::cerr << "  libbpf btf dump: "
-#ifdef HAVE_LIBBPF_BTF_DUMP
-            << "yes" << std::endl;
-#else
-            << "no" << std::endl;
-#endif
-  std::cerr << "  libbpf btf dump type decl: "
-#ifdef HAVE_LIBBPF_BTF_DUMP_EMIT_TYPE_DECL
-            << "yes" << std::endl;
-#else
-            << "no" << std::endl;
-#endif
+  std::cerr << std::endl;
+  std::cerr << BuildInfo::report();
 
   std::cerr << std::endl;
-
   std::cerr << BPFfeature().report();
 
   return 0;